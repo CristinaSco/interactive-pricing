676baccb2941fac3e138a29b80926914ce3b37bf
Cristina Scognamiglio <77617593+CristinaSco@users.noreply.github.com>
2021-04-16 14:07:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -39,16 +39,3 @@ rangeslider.oninput = function() {
     progressBar.style.width = "100%";
   }
 }
-
-function calc(){
-
-  if (document.getElementById('checkboxRange').checked){
-      document.getElementById('sliderRange').min = 0;
-      document.getElementById('sliderRange').max = 80;
-      document.getElementById('sliderRange').step = 20;
-  } else {
-    document.getElementById('sliderRange').min = 0;
-    document.getElementById('sliderRange').max = 64;
-    document.getElementById('sliderRange').step = 16;
-  }
-}
